63054dca97cf8649f9d9c2f52856106a3391c9aa
kawaAAT <bogdankawaat@gmail.com>
2020-12-09 17:17:33 +0200

    Added homework functionality

--- TestCpp/TestCpp.cpp
+++ TestCpp/TestCpp.cpp
@@ -1,20 +1,24 @@
-// TestCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<int> v = { 7, 9, 1, 7, 8, 0, 9, 1, 8 };
+
+    for (int i = 0; i < v.size(); i++) {
+        int& controlNumber = v[i];
+        int currentDuplicates = 0;
+
+        for (int j = 0; j < v.size(); j++) {
+            int& checkingNumber = v[j];
+
+            if (checkingNumber == controlNumber && i != j) {
+                currentDuplicates++;
+            }
+        }
+
+        if (currentDuplicates == 0) {
+            std::cout << controlNumber << std::endl;
+        }
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
